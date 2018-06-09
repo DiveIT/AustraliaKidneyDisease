3e98f501a6fd7acf93cba68f695fdfd031b12184
Sarah Hepworth <sarahhepworth@Sarahs-iMac.local>
2018-06-09 16:38:37 +1000

    update plots

--- Survey-Data/explore_survey.R
+++ Survey-Data/explore_survey.R
@@ -23,31 +23,19 @@ set_wd()
 
 # Import data ----
 # Limited information is available. Data is sparse.
-# Review each dataset for 
-survey_geo <- read.csv("KidneyStateRemoteness.csv")
-survey_strata <- read.csv("KidneySexStateRemote.csv")
-survey_state <- read.csv("KidneySexState.csv")
-survey_age <- read.csv("KidneySexAge.csv")
-survey_incm <- read.csv("KidneySexIncome.csv")
-survey_incm <- read.csv("KidneyStateIncome.csv")
-survey_phn <- read.csv("KidneyPHN.csv")
-
+# Indigenous Health Survey
+survey_age <- read.csv("./data/KidneyAge.csv")
+survey_geo <- read.csv("./data/KidneyStateRemoteness.csv")
+survey_incm <- read.csv("./data/KidneyIncome.csv")
+# Australian Health Survey
+all_survey_incm <- read.csv("./data/AllKidneyIncome.csv")
+all_survey_geo <- read.csv("./data/AllKidneyStateRemoteness.csv")
 
 # Transform data ----
-survey_geo$Remoteness <- as.character(survey_geo$Remoteness)
-survey_geo$Remoteness[survey_geo$Remoteness =="City"] <- "Major Cities of Australia"
-survey_geo$Remoteness[survey_geo$Remoteness =="InnerRegional"] <- "Inner Regional Australia"
-survey_geo$Remoteness[survey_geo$Remoteness =="OuterRegional"] <- "Outer Regional Australia"
-survey_geo$Remoteness[survey_geo$Remoteness =="Remote"] <- "Remote Australia"
-survey_geo$Remoteness[survey_geo$Remoteness =="VeryRemote"] <- "Very Remote Australia"
 survey_geo <- survey_geo %>% 
-  mutate(Total = Not.Reported+Kidney.Disease+No.Kidney.Disease,
-         Geography = paste0(State,"-",Remoteness))
+  mutate(Geography = paste0(State,"-",Remoteness))
 
-
-
-survey_strata <- survey_strata %>% 
-  mutate(Total = Kidney.Disease+No.Kidney.Disease)
+survey_strata <- survey_geo 
 survey_strata$Remoteness <- as.character(survey_geo$Remoteness)
 survey_strata$Remoteness[survey_geo$Remoteness =="Major Cities of Australia"] <- 1
 survey_strata$Remoteness[survey_geo$Remoteness =="Inner Regional Australia"] <-2
@@ -58,12 +46,18 @@ survey_strata <- survey_strata %>%
   mutate(Remoteness=as.numeric(Remoteness),
          ppt=Kidney.Disease/Total*1000)
 
+survey_geo <- survey_geo %>% 
+  mutate(Geography = paste0(State,"-",Remoteness))
 
+all_survey_geo <- all_survey_geo %>% 
+  mutate(Geography = paste0(State,"-",Remoteness))
 
+#Age into deciles
 survey_age <- survey_age %>% 
-  mutate(Total = Not.reported+Kidney.Disease+No.Kidney.Disease)
+  mutate(Total = Kidney.Disease+No.Kidney.Disease)
 survey_age$Age <- as.character(survey_age$Age)
-survey_age$Age[survey_age$Age %in% c("18","19")] <- "10-19 years"
+survey_age$Age[survey_age$Age %in% c("1","2","3","4","5","6","7","8","9")] <- "0-9 years"
+survey_age$Age[survey_age$Age %in% c("10","11","12","13","14","15","16","17","18","19")] <- "10-19 years"
 survey_age$Age[survey_age$Age %in% c("20","21","22","23","24","25","26","27","28","29")] <- "20-29 years"
 survey_age$Age[survey_age$Age %in% c("30","31","32","33","34","35","36","37","38","39")] <- "30-39 years"
 survey_age$Age[survey_age$Age %in% c("30","31","32","33","34","35","36","37","38","39")] <- "30-39 years"
@@ -83,7 +77,23 @@ survey_age$Age[survey_age$Age %in% c("90","91","92","93","94","95","96","97","98
 # $1,250-$1,499 ($1,331-$1,663)
 # $1,500-$1,999 ($1,664-$2,027)
 # $2,000 or more ($2,028 or more)
+
 survey_incm$incm <- as.character(survey_incm$Gross.weekly.cash.income.of.household..deciles)
+survey_incm$deciles <- NA
+survey_incm$deciles[survey_incm$incm =="Not known"] <- 0
+survey_incm$deciles[survey_incm$incm =="First decile"] <- 1
+survey_incm$deciles[survey_incm$incm =="Second decile"] <- 2
+survey_incm$deciles[survey_incm$incm =="Third decile"] <- 3
+survey_incm$deciles[survey_incm$incm =="Fourth decile"] <- 4
+survey_incm$deciles[survey_incm$incm =="Fifth decile"] <- 5
+survey_incm$deciles[survey_incm$incm =="Sixth decile"] <- 6
+survey_incm$deciles[survey_incm$incm =="Seventh decile"] <- 7
+survey_incm$deciles[survey_incm$incm =="Eighth decile"|survey_incm$incm =="Ninth decile"|survey_incm$incm =="Tenth decile"] <- 8
+survey_incm <- survey_incm %>% 
+  filter(incm!="Not applicable") 
+survey_incm <- survey_incm %>% 
+  filter(incm!="Not stated")
+
 survey_incm$incm[survey_incm$incm =="First decile"] <- "$1-$299"
 survey_incm$incm[survey_incm$incm =="Second decile"] <- "$300-$399"
 survey_incm$incm[survey_incm$incm =="Third decile"] <- "$400-$799"
@@ -93,24 +103,75 @@ survey_incm$incm[survey_incm$incm =="Sixth decile"] <- "$1,250-$1,499"
 survey_incm$incm[survey_incm$incm =="Seventh decile"] <- "$1,500-$1,999"
 survey_incm$incm[survey_incm$incm =="Eighth decile"|survey_incm$incm =="Ninth decile"|survey_incm$incm =="Tenth decile"] <- "$2,000 or more"
 survey_incm <- survey_incm %>% 
-  mutate(Total = Not.reported+Kidney.Disease+No.Kidney.Disease)
+  select(incm,deciles,Kidney.Disease)
 
 
-survey_phn <- survey_phn %>% 
-  mutate(Total = Not.Reported+Kidney.Disease+No.Kidney.Disease)
+#Total Kidney Disease data
+all_survey_incm$incm <- as.character(all_survey_incm$Gross.weekly.cash.income.of.household..deciles)
+all_survey_incm$deciles <- NA
+all_survey_incm$deciles[all_survey_incm$incm =="Not known"] <- 0
+all_survey_incm$deciles[all_survey_incm$incm =="First decile"] <- 1
+all_survey_incm$deciles[all_survey_incm$incm =="Second decile"] <- 2
+all_survey_incm$deciles[all_survey_incm$incm =="Third decile"] <- 3
+all_survey_incm$deciles[all_survey_incm$incm =="Fourth decile"] <- 4
+all_survey_incm$deciles[all_survey_incm$incm =="Fifth decile"] <- 5
+all_survey_incm$deciles[all_survey_incm$incm =="Sixth decile"] <- 6
+all_survey_incm$deciles[all_survey_incm$incm =="Seventh decile"] <- 7
+all_survey_incm$deciles[all_survey_incm$incm =="Eighth decile"|all_survey_incm$incm =="Ninth decile"|all_survey_incm$incm =="Tenth decile"] <- 8
+all_survey_incm <- all_survey_incm %>% 
+  filter(incm!="Not applicable") 
+all_survey_incm <- all_survey_incm %>% 
+  filter(incm!="Not stated")
 
+all_survey_incm$incm[all_survey_incm$incm =="First decile"] <- "$1-$299"
+all_survey_incm$incm[all_survey_incm$incm =="Second decile"] <- "$300-$399"
+all_survey_incm$incm[all_survey_incm$incm =="Third decile"] <- "$400-$799"
+all_survey_incm$incm[all_survey_incm$incm =="Fourth decile"] <- "$800-$999"
+all_survey_incm$incm[all_survey_incm$incm =="Fifth decile"] <- "$1,000-$1,249"
+all_survey_incm$incm[all_survey_incm$incm =="Sixth decile"] <- "$1,250-$1,499"
+all_survey_incm$incm[all_survey_incm$incm =="Seventh decile"] <- "$1,500-$1,999"
+all_survey_incm$incm[all_survey_incm$incm =="Eighth decile"|all_survey_incm$incm =="Ninth decile"|all_survey_incm$incm =="Tenth decile"] <- "$2,000 or more"
+all_survey_incm <- all_survey_incm %>% 
+  select(incm,deciles,All.Kidney.Disease)
 
 # Summarise data ----
 # For all survey sets
-dim(survey_ds)
+dim(survey_age)
+vis_dat(survey_age) #factor and numeric features
+#investigate missing values by setting 0 to NA
+survey_age_miss <- survey_age %>% 
+  mutate(Kidney.Disease = ifelse(Kidney.Disease==0, NA, Kidney.Disease))
+vis_miss(survey_age_miss) # 10.1% 0 values
+summary(survey_age)
 
+dim(survey_geo)
+vis_dat(survey_geo) #factor and numeric features
+survey_geo_miss <- survey_geo %>% 
+  mutate(Kidney.Disease = ifelse(Kidney.Disease==0, NA, Kidney.Disease))
+vis_miss(survey_geo_miss) # 9.1% 0 values
+summary(survey_geo)
+
+dim(survey_incm)
+vis_dat(survey_incm) #factor and numeric features
+survey_incm_miss <- survey_incm %>% 
+  mutate(Kidney.Disease = ifelse(Kidney.Disease==0, NA, Kidney.Disease))
+vis_miss(survey_incm_miss) # 6.2% 0 values
+summary(survey_incm)
+
+describe(survey_age$Kidney.Disease, IQR=TRUE) 
+describe(survey_geo$Kidney.Disease, IQR=TRUE) 
+describe(survey_incm$Kidney.Disease, IQR=TRUE) 
+
+
+
+
+dim(survey_ds)
 vis_dat(survey_ds) #factor and numeric features
 vis_miss(survey_ds) #no data missing
-
 summary(survey_ds)
 
 describe(survey_ds$Not.reported, IQR=TRUE) 
-describe(survey_ds$Kidney.Disease, IQR=TRUE) 
+describe(survey_incm$Kidney.Disease, IQR=TRUE) 
 describe(survey_ds$No.Kidney.Disease, IQR=TRUE) 
 
 #investigate missing values by setting 0 to NA
@@ -121,70 +182,60 @@ survey_ds_miss <- survey_ds %>%
 
 vis_miss(survey_ds_miss) # 12% 0 values
 
-# TODO: Plot values
-## Map survey ----
+
+
+# Histograms of data ----
+#incm
+survey_incm$deciles <- reorder(survey_incm$incm, survey_incm$deciles)
+g <- ggplot(survey_incm, aes(deciles,Kidney.Disease))
+g + geom_bar(stat="identity", colour="black") + 
+  labs(title="Indigenous persons with Kidney disease", 
+       subtitle="Distribution of Weekly Income level") + theme_classic()
+
+all_survey_incm$deciles <- reorder(all_survey_incm$incm, all_survey_incm$deciles)
+g <- ggplot(all_survey_incm, aes(deciles,All.Kidney.Disease))
+g + geom_bar(stat="identity", colour="black") + 
+  labs(title="All persons with Kidney disease", 
+       subtitle="Distribution of Weekly Income level") + theme_classic()
+
+
+incm_plot <- left_join(survey_incm,all_survey_incm, by=c("incm","deciles"))
+
+incm_plot <- incm_plot %>% gather("Type","Kidney.Disease",c(Kidney.Disease,All.Kidney.Disease))
+totals = incm_plot %>% group_by(Type) %>% summarise(total=sum(Kidney.Disease))
+incm_plot <- left_join(incm_plot,totals, by=c("Type"))
+incm_plot <- incm_plot %>% mutate(Kidney.Disease = Kidney.Disease/total)
+incm_plot$deciles <- reorder(incm_plot$incm, incm_plot$deciles)
+incm_plot <- incm_plot %>% select(incm,Type,Kidney.Disease)
+ggplot(incm_plot, aes(x=incm,y=Kidney.Disease, colour=Type)) + geom_bar(stat="identity") + 
+  labs(title="Persons with Kidney disease", 
+       subtitle="Distribution of Weekly Income level") + theme_classic()
+
+# age
+ggplot(survey_age, aes(Age,Kidney.Disease)) + geom_bar(stat="identity", fill="#999999") + 
+  labs(title="Indigenous Persons with Kidney disease", 
+       subtitle="Age distribution") + theme_classic()
+
+#remoteness - leaflet map
 library(sp)
 library(rgdal)
 library(broom)
 library(data.table)
 library(leaflet)
 
-phn_poly = readOGR("/Users/sarahhepworth/Documents/R Sandbox/data/phn_shape/PHN_boundaries_AUS_Sep2015_V5.shp", stringsAsFactors=FALSE)
 remote_poly = readOGR("/Users/sarahhepworth/Documents/R Sandbox/data/remoteness_shape/RA_2011_AUST.shp", stringsAsFactors=FALSE)
-
 remote_poly@data$STE_RA = paste0(remote_poly@data$STE_NAME11,"-",remote_poly@data$RA_NAME11)
 
 # Join the CSV data to the shapefile:
-survey_poly = merge(x=phn_poly, y=survey_phn, by.x="PHN_Name", by.y="PHN")
 survey_remote_poly = merge(x=remote_poly, y=survey_geo, by.x="STE_RA",by.y="Geography")
 
 #proj4string(survey_phn) <- CRS("+init=epsg:4283")
 #survey_poly2 <- spTransform(survey_phn, prj.LatLong)
 
-# Extracting point locations for each polygon 
-phn_poly@data$id = rownames(phn_poly@data)
-phn_poly.points = tidy(phn_poly)
-phn_poly.df = inner_join(phn_poly.points, phn_poly@data, by = "id")
-phn_poly.df = subset(phn_poly.df, select = c(long, lat, group, PHN_Name))
-names(phn_poly.df) = c("long", "lat", "group", "PHN_Name")
-
-survey.data = inner_join(survey_phn, phn_poly.df, by = c("PHN"="PHN_Name"))
-
-#Transform to take centroid (for fast processing)
-survey.data.point <- survey.data %>% 
-  group_by(PHN, Kidney.Disease) %>% 
-  summarise(long=median(long),
-            lat=median(lat))
-
-label_points = coordinates(survey_poly)
-x_label = label_points[,1]
-y_label = label_points[,2]
-label_text = sprintf("%s", survey.data$PHN)
-
-labels.df = data.frame(x_label, y_label, label_text, stringsAsFactors=FALSE)
-
-
-
 ## define a palette for the colour
-pal <- colorNumeric(palette = "YlOrRd",
+pal <- colorNumeric(palette = "PuRd",
                     domain = survey_remote_poly$Kidney.Disease)
-
-leaflet(data = survey.data.point) %>%
-  addTiles() %>%
-  addCircleMarkers(lat = ~lat, lng = ~long, popup = ~PHN, 
-                   color = ~pal(Kidney.Disease), stroke = FALSE, fillOpacity = 0.6) %>%
-  addLegend(position = "bottomleft", pal = pal, values = ~Kidney.Disease)
-
 #polygons
-leaflet(data=survey_poly) %>%
-  addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
-              opacity = 1.0, fillOpacity = 0.5,
-              fillColor = ~pal(Kidney.Disease),
-              highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                  bringToFront = TRUE)) %>%
-  addLegend(position = "bottomleft", pal = pal, values = ~Kidney.Disease) 
-
-
 leaflet(data=survey_remote_poly) %>%
   addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
               opacity = 1.0, fillOpacity = 0.5,
@@ -192,6 +243,56 @@ leaflet(data=survey_remote_poly) %>%
               highlightOptions = highlightOptions(color = "white", weight = 2,
                                                   bringToFront = TRUE)) %>%
   addLegend(position = "bottomleft", pal = pal, values = ~Kidney.Disease) 
+
+# Extracting point locations for each polygon 
+#remote_poly@data$id = rownames(remote_poly@data)
+#remote_poly.points = tidy(remote_poly)
+#remote_poly.df = inner_join(remote_poly.points, remote_poly@data, by = "id")
+#remote_poly.df = subset(remote_poly.df, select = c(long, lat, group, STE_RA))
+#names(remote_poly.df) = c("long", "lat", "group", "STE_RA")
+
+#survey.data = merge(survey_remote_poly, remote_poly.df, by = c("STE_RA"="STE_RA"))
+
+#Transform to take centroid (for fast processing)
+#survey.data.point <- survey.data %>% 
+#  group_by(PHN, Kidney.Disease) %>% 
+#  summarise(long=median(long),
+ #           lat=median(lat))
+
+#label_points = coordinates(survey_poly)
+#x_label = label_points[,1]
+#y_label = label_points[,2]
+#label_text = sprintf("%s", survey.data$PHN)
+
+#labels.df = data.frame(x_label, y_label, label_text, stringsAsFactors=FALSE)
+
+
+#leaflet(data = survey.data.point) %>%
+#  addTiles() %>%
+#  addCircleMarkers(lat = ~lat, lng = ~long, popup = ~PHN, 
+#                   color = ~pal(Kidney.Disease), stroke = FALSE, fillOpacity = 0.6) %>%
+#  addLegend(position = "bottomleft", pal = pal, values = ~Kidney.Disease)
+
+
+#compare to all data distribution
+# Join the CSV data to the shapefile:
+all_survey_remote_poly = merge(x=remote_poly, y=all_survey_geo, by.x="STE_RA",by.y="Geography")
+
+#proj4string(survey_phn) <- CRS("+init=epsg:4283")
+#survey_poly2 <- spTransform(survey_phn, prj.LatLong)
+
+## define a palette for the colour
+pal <- colorNumeric(palette = "PuRd",
+                    domain = all_survey_remote_poly$Kidney.Disease)
+#polygons
+leaflet(data=all_survey_remote_poly) %>%
+  addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.5,
+              fillColor = ~pal(Kidney.Disease),
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE)) %>%
+  addLegend(position = "bottomleft", pal = pal, values = ~Kidney.Disease) 
+
 
 
 # TODO: Select most appropriate data
